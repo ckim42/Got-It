68aeffb371a4795486d03c882adf2d19798e8b7a
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-01-15 15:16:59 -0800

    heroku

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const app = express();
+const port = process.env.PORT || 5000;
 
 // middleware
-mongoose.connect('mongodb://localhost/got-it');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/got-it');
 app.use(methodOverride('_method'))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(port);
 
 // models
 const Entry = require('./models/entry');
@@ -22,11 +24,6 @@ const entries = require('./controllers/entries');
 entries(app);
 const comments = require('./controllers/comments');
 comments(app);
-
-// check web server
-app.listen(3000, () => {
-  console.log('App listening on port 3000!')
-});
 
 module.exports = app;
 
