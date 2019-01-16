5ab6e7413f24f5152a22eb53ccaf30c041eccaf1
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-01-15 16:33:40 -0800

    herokuuuu

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
-const app = express();
+const index = express();
 
 // middleware
-app.use(methodOverride('_method'))
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.use(bodyParser.urlencoded({ extended: true }));
+index.use(methodOverride('_method'))
+index.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+index.set('view engine', 'handlebars');
+index.use(bodyParser.urlencoded({ extended: true }));
 
 // models
 const Entry = require('./models/entry');
@@ -19,16 +19,16 @@ const Comment = require('./models/comment');
 // controllers
 const entries = require('./controllers/entries');
 const comments = require('./controllers/comments');
-entries(app);
-comments(app);
+entries(index);
+comments(index);
 
 const port = process.env.PORT || 5000
 // mongoose connection
 const mongoUri = process.env.MONGODB_URI || "mongodb://localhost.27017/got_it"; mongoose.connect(mongoUri, { useNewUrlParser: true } );
 
 // web server check
-app.listen(port, () => {
-  console.log('App listening on port 5000!')
+index.listen(port, () => {
+  console.log('index listening on port 5000!')
 });
 
-module.exports = app;
+module.exports = index;
