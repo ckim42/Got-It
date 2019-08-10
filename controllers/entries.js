06ba671155f4c60a7a7be4c209bf642a38cedf21
ilikecoffee18 <cherish.kim@students.makeschool.com>
2019-08-09 18:33:06 -0700

    top used tags for each rating can now display

--- controllers/entries.js
+++ controllers/entries.js
@@ -58,9 +58,9 @@ module.exports = (app) => {
   // Index/Read - for all entries w/ same TAG
   // ❌ 만지지마!!!!
   app.get('/tags/:tag', (req, res) => {
-    Entry.find({ tagsString: { $all: [req.params.tag] } })
+    Entry.find({tagsString: {$all: [req.params.tag]}})
       .then(async entries => { //staggered stuff not all at once = async
-        const tagHunt = await Tag.find({ tagName: req.params.tag }).then(tags => { //await the thing we need to continue
+        const tagHunt = await Tag.find({tagName: req.params.tag}).then(tags => { //await the thing we need to continue
           let addedRatings = 0 //this number isn't fixed
           let averageRating = 0 //ditto
           const numEntries = tags.length //this number IS fixed
@@ -70,66 +70,53 @@ module.exports = (app) => {
           averageRating = addedRatings / numEntries
           return averageRating
         })
-      res.render('tagged-entries', { entries: entries, tag: req.params.tag, average: tagHunt })
+      res.render('tagged-entries', {entries: entries, tag: req.params.tag, average: tagHunt})
       // console.log(tagHunt)
     }).catch(err => {
       console.log(err.message);
     })
   })
 
-  // 😁 super similar to the above, yo!!!
   // Index/Read - for all entries w/ same RATING
   app.get('/ratings/:rating', (req, res) => {
-    let allTags = [] //GOOD!
-    let tagsDict = {}
-    // let topTags = []
-    Entry.find({ rating: req.params.rating })
+    Entry.find({rating: req.params.rating})
       .then(async entries => {
         const ratingHunt = await Rating.find({ratingNum: req.params.rating}).then(ratings => {
-          // for (const rating of ratings) {
-          //   for (const tag of rating.tagsUsed) {
-          //     allTags.push(tag)
-          //     console.log(allTags)
-          //     for (const thing of allTags) {
-          //       if (thing in tagsDict) {
-          //         tagsDict[thing] += 1 //no dot notation :( cuz it's a var
-          //       } else {
-          //         tagsDict[thing] = 1
-          //       }
-          //     }
-          //   }
-          // }
-
-          for (const rating of ratings) { //GOOD!
+          let allTags = []
+          let tagsDict = {}
+          let placeHold = []
+          let topTags = []
+          for (const rating of ratings) {
             for (const tag of rating.tagsUsed) {
               allTags.push(tag)
             }
           }
-
-          for (const thing of allTags) {
-            if (thing in tagsDict) {
-              tagsDict[thing] += 1
+          for (const tag of allTags) {
+            if (tag in tagsDict) {
+              tagsDict[tag] += 1 //no dot notation - 'thing' is a var
             } else {
-              tagsDict[thing] = 1
+              tagsDict[tag] = 1
             }
           }
-          // for (const thing of allTags) {
-          //   if (thing in tagsDict) {
-          //     tagsDict[thing] += 1 //no dot notation b/c 'thing' is a var
-          //   } else {
-          //     tagsDict[thing] = 1
-          //   }
-          // }
-          console.log('allTags' + allTags)
-          return tagsDict
-          // var standIn = Object.keys(tagsDict).map(function(key) { //create items array
-          //   return [key, tagsDict[key]]
-          // })
-          // standIn.sort(function(first, second) { //sort array based on 2nd element
-          //   return second[1] - first[1]
-          // })
-          // return standIn.slice(0, 3) //return only the first 3 items
-          // // return topTags
+          for (const key in tagsDict) {
+            if (tagsDict.hasOwnProperty(key)) {
+              placeHold.push([key, tagsDict[key]])
+            }
+          }
+          placeHold.sort(function(a, b) {return b[1] - a[1]})
+          if (placeHold.length == 0) {
+            console.log('Uhm looks like you\'re doing something wrong there')
+          } else if (placeHold.length == 1) {
+            topTags.push(placeHold[0][0])
+          } else if (placeHold.length == 2) {
+            topTags.push(placeHold[0][0])
+            topTags.push(placeHold[1][0])
+          } else {
+            topTags.push(placeHold[0][0])
+            topTags.push(placeHold[1][0])
+            topTags.push(placeHold[2][0])
+          }
+          return topTags
         })
       console.log(ratingHunt)
       res.render('rated-entries', { entries: entries, rating: req.params.rating, topThreeTags: ratingHunt })
